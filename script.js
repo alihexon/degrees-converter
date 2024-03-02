3a379db5f1231a532a9f179cf9e391f8d0441264
hexoN <aliakbaripour00@gmail.com>
2024-03-02 14:46:16 +0330

    Add functions Add convertToFahrenheit function Add convertToCelsius function Add converTemperature function Call all the functions

---
+++ script.js
@@ -0,0 +1,26 @@
+function convertToFahrenheit(celsius) {
+  const convertedCelsius = (celsius * 9 / 5) + 32;
+  return convertedCelsius;
+}
+
+function convertToCelsius(fahrenheit) {
+  const convertedFahrenheit = (fahrenheit - 32) * 5 / 9;
+  return convertedFahrenheit;
+}
+
+function converTemperature(degrees, unit) {
+  if (unit === 'c') {
+    const convertedCelsius = (degrees * 9 / 5) + 32;
+    return convertedCelsius;
+  } else if (unit === 'f') {
+    const convertedFahrenheit = (degrees - 32) * 5 / 9;
+    return convertedFahrenheit;
+  } else {
+    console.log('Either the degrees or the unit if undefined')
+  }
+}
+
+console.log(convertToFahrenheit(25));
+console.log(convertToCelsius(86));
+console.log(converTemperature(25, 'c'));
+console.log(converTemperature(86, 'f'));
