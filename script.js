e205660b72a362dad3e52d0f778d66a17cacb131
hexoN <aliakbaripour00@gmail.com>
2024-03-13 22:08:11 +0330

    config(title): Renamed HTML title element

--- script.js
+++ script.js
@@ -7,13 +7,20 @@ let result;
 
 function convertTemperature() {
  if (convertOption.value === 'c') {
-   console.log(((temperatureNumInput.value * 9 / 5) + 32).toFixed(1));
+   result = (((temperatureNumInput.value * 9 / 5) + 32).toFixed(1));
   } else if (convertOption.value === 'f') {
-   console.log(((temperatureNumInput.value - 32) * 5 / 9).toFixed(4));
+   result = (((temperatureNumInput.value - 32) * 5 / 9).toFixed(4));
  }
-
- temperatureInput.value = '';
+ displayResult();
  temperatureNumInput.value = '';
+}
+
+function displayResult() {
+  if (convertOption.value === 'c') {
+    resultParagraph.innerText = `${temperatureNumInput.value} Celsius is equal to ${result} Fahrenheit`;
+   } else if (convertOption.value === 'f') {
+    resultParagraph.innerText = `${temperatureNumInput.value} Fahrenheit is equal to ${result} Celsius`;
+  }
 }
 
 convertButton.addEventListener('click', convertTemperature)
