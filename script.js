d1e5ee84f51ae74c1985301483a87a8768350b5e
hexoN <aliakbaripour00@gmail.com>
2024-03-13 22:32:17 +0330

    hot-fix(toF-not-working): Fixed a bug where toF result wasn't correct. (I messed up with the last commit so yeah 🤦‍♂️)

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 const temperatureNumInput = document.querySelector('.js-temp-input');
-const temperatureInput = document.querySelector('.temperature-sel');
-const convertOption = document.querySelector('.option');
+const convertOption = document.querySelector('.temperature-sel');
 const convertButton = document.querySelector('.convert-btn');
 let resultParagraph = document.querySelector('.result');
 let result;
 
 function convertTemperature() {
- if (convertOption.value === 'c') {
-   result = (((temperatureNumInput.value * 9 / 5) + 32).toFixed(1));
-  } else if (convertOption.value === 'f') {
-   result = (((temperatureNumInput.value - 32) * 5 / 9).toFixed(4));
+ if (convertOption.value === 'toC') {
+   result = (((temperatureNumInput.value - 32) * 5 / 9).toFixed(3));
+   resultParagraph.innerText = `${temperatureNumInput.value} Fahrenheit is equal to ${result} Celsius`;
+  } else if (convertOption.value === 'toF') {
+    result = (((temperatureNumInput.value * 9 / 5) + 32).toFixed(1));
+    resultParagraph.innerHTML = `${temperatureNumInput.value} Celsius is equal to ${result} Fahrenheit`;
  }
- displayResult();
  temperatureNumInput.value = '';
-}
-
-function displayResult() {
-  if (convertOption.value === 'c') {
-    resultParagraph.innerText = `${temperatureNumInput.value} Celsius is equal to ${result} Fahrenheit`;
-   } else if (convertOption.value === 'f') {
-    resultParagraph.innerText = `${temperatureNumInput.value} Fahrenheit is equal to ${result} Celsius`;
-  }
 }
 
 convertButton.addEventListener('click', convertTemperature)
