87349f105e1d9a5f90d86dee043fd9e9a37548c9
hexoN <aliakbaripour00@gmail.com>
2024-03-13 21:42:41 +0330

    chore(variables): Added variables and fixed their HTML

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-function convertToFahrenheit(celsius) {
-  const convertedCelsius = (celsius * 9 / 5) + 32;
-  return convertedCelsius;
-}
+const temperatureNumInput = document.querySelector('.js-temp-input')
+const temperatureInput = document.querySelector('.temperature-sel')
+const convertButton = document.querySelector('.convert-btn')
+let resultParagraph = document.querySelector('.result')
 
-function convertToCelsius(fahrenheit) {
-  const convertedFahrenheit = (fahrenheit - 32) * 5 / 9;
-  return convertedFahrenheit;
-}
+// function convertToFahrenheit(celsius) {
+//   const convertedCelsius = (celsius * 9 / 5) + 32;
+//   return convertedCelsius;
+// }
 
-function converTemperature(degrees, unit) {
-  if (unit === 'c') {
-    const convertedCelsius = (degrees * 9 / 5) + 32;
-    return convertedCelsius;
-  } else if (unit === 'f') {
-    const convertedFahrenheit = (degrees - 32) * 5 / 9;
-    return convertedFahrenheit;
-  } else {
-    console.log('Either the degrees or the unit if undefined')
-  }
-}
+// function convertToCelsius(fahrenheit) {
+//   const convertedFahrenheit = (fahrenheit - 32) * 5 / 9;
+//   return convertedFahrenheit;
+// }
 
-console.log(convertToFahrenheit(25));
-console.log(convertToCelsius(86));
-console.log(converTemperature(25, 'c'));
-console.log(converTemperature(86, 'f'));
+// function converTemperature(degrees, unit) {
+//   if (unit === 'c') {
+//     const convertedCelsius = (degrees * 9 / 5) + 32;
+//     return convertedCelsius;
+//   } else if (unit === 'f') {
+//     const convertedFahrenheit = (degrees - 32) * 5 / 9;
+//     return convertedFahrenheit;
+//   } else {
+//     console.log('Either the degrees or the unit if undefined')
+//   }
+// }
+
+// console.log(convertToFahrenheit(25));
+// console.log(convertToCelsius(86));
+// console.log(converTemperature(25, 'c'));
+// console.log(converTemperature(86, 'f'));
